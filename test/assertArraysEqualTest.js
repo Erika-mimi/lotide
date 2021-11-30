a70f545b2d93a7d93666d4d17ae8f4f2d739ddcf
Erika-mimi <erikayoshida5@gmail.com>
2021-11-30 05:44:04 +0000

    Added assertArraysEqual.js using require and exports

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+//Require 2 functions
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) 
